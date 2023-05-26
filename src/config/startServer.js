6f3f154af38e98575f3afe33dadd54511a447552
Wolleys Migaya <wolleysmigaya@gmail.com>
2023-05-26 08:11:55 +0300

    Setup a bare structure for swagger documentation

--- src/config/startServer.js
+++ src/config/startServer.js
@@ -1,11 +1,13 @@
 const { connect } = require("./dbConn");
 const PORT = process.env.PORT || 3300;
+const { swaggerDocs: V1SwaggerDocs } = require("../v1/swagger");
 
-// Start server and connect to MySQL DB
+// Start server, spin up swagger documentation and connect to MySQL DB
 const startServer = (app) => {
   try {
     app.listen(PORT, () => {
       console.log("SERVER: started on port", PORT);
+      V1SwaggerDocs(app, PORT);
       setTimeout(connect, 1000);
     });
   } catch (error) {
